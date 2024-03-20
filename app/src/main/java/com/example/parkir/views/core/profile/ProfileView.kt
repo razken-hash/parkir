50b3ec11c30a459a0fd216df773b073cf5221362
razken-hash <ka_kenniche@esi.dz>
2024-03-20 23:35:03 +0100

    ProfileScreen Implemented

---
+++ app/src/main/java/com/example/parkir/views/core/profile/ProfileView.kt
@@ -0,0 +1,23 @@
+package com.example.parkir.views.core.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeView(navController: NavHostController) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "HomeView", style = MaterialTheme.typography.displayLarge)
+    }
+}
