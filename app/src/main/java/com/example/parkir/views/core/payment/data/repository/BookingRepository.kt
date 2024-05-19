c0a401aa861d787bf5643e1a89fc2b2e5cecab5d
razken-hash <ka_kenniche@esi.dz>
2024-05-19 21:22:15 +0100

    Bookings Data Layer + Payments Data Layer Added

--- app/src/main/java/com/example/parkir/views/core/payment/data/repository/BookingRepository.kt
+++ app/src/main/java/com/example/parkir/views/core/payment/data/repository/BookingRepository.kt
@@ -1,6 +1,7 @@
 package com.example.parkir.views.core.payment.data.repository
 
-import com.example.parkir.views.core.bookmarks.data.services.PaymentService
+import com.example.parkir.views.core.payment.data.services.PaymentService
+
 
 class PaymentRepository(
     private val paymentService: PaymentService
